bdb62e9c79568b88f33d48d75998bd0e9ddbbd75
Paetin Nelson <paetin.nelson@gmail.com>
2025-12-29 03:51:18 +0000

    wip

--- lua/pathfinder/init.lua
+++ lua/pathfinder/init.lua
@@ -2,8 +2,14 @@ local M = {}
 
 function M.startPathfinder()
 	local buf = vim.api.nvim_create_buf(false, true)
-	vim.api.nvim_open_win(buf, false,
-		{ relative = 'win', row = 3, col = 3, width = 12, height = 3 })
+	local width = math.floor(vim.o.columns * .5)
+	local height = math.floor(vim.o.lines * .5)
+	local row = math.floor(height / 2)
+	local col = math.floor(width / 2)
+
+	vim.api.nvim_open_win(buf, true,
+		{ relative = 'editor', row = row, col = col, width = width, height = height })
+	vim.api.nvim_set_current_buf(buf)
 end
 
 vim.api.nvim_buf_create_user_command(0, "Pathfinder", function()
