33c4de15f7b7f5c22727d86f8ac04b71dedee9d1
Paetin Nelson <paetin.nelson@gmail.com>
2025-12-29 03:32:42 +0000

    wip

--- lua/pathfinder/init.lua
+++ lua/pathfinder/init.lua
@@ -1,11 +1,12 @@
 local M = {}
 
 function M.startPathfinder()
-	print("starting pathfinder")
+	vim.api.nvim_open_win(0, false,
+		{ relative = 'win', row = 3, col = 3, width = 12, height = 3 })
 end
 
 vim.api.nvim_buf_create_user_command(0, "Pathfinder", function()
 	M.startPathfinder()
-end, { desc = "Starting Pathfinder"})
+end, { desc = "Starting Pathfinder" })
 
 return M
